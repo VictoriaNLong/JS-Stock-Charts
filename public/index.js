b3c61aa661bc5014508cfc5d4d121cd1ee834ddf
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-02-19 11:40:24 -0800

    final

--- public/index.js
+++ public/index.js
@@ -4,6 +4,84 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+//    const stockInfo = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min', {
+//     method: "GET",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Authorization": "apikey 77545fbbdb1d4000a5bd6dab6305e3a1"
+//     }, 
+//    })
+
+//    const si = await stockInfo.json()
+//    console.log(si)
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+stocks.forEach(stock => stock.values.reverse())
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map( stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.map(value => parseFloat(value.high)),
+            backgroundColor:  getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+const maxStockValues = stockArr => {
+    const finalObj = {};
+    stockArr.forEach(stock => {
+      const listOfHighValues = stock.values.map(day => {
+        return Number(day.high)
+      });
+      finalObj[stock.meta.symbol] = Math.max(...listOfHighValues);
+    })
+  
+    return finalObj;
+  }
+
+  const myStockValues = maxStockValues(stocks)
+
+new Chart(highestPriceChartCanvas.getContext('2d'), {
+    type: 'bar',
+    data: {
+        labels: stocks.map(stock => stock.meta.symbol),
+        datasets: [{
+        label: 'highest',
+        data: myStockValues,
+        backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+        borderColor: stocks.map(stock => getColor(stock.meta.symbol)),
+
+    }]
+        // stocks.map( stock => ({
+        //     label: 'Highest',
+        //     data: myStockValues[stock.meta.symbol],
+        //     backgroundColor:  getColor(stock.meta.symbol),
+        //     borderColor: getColor(stock.meta.symbol),
+        // }))
+    }
+});
+  
 }
 
 main()
